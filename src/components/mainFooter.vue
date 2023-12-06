af9a8d6be2a7e73db696f1ee1c435b2c9b11e98e
Samb55 <samb_55@hotmail.com>
2023-12-06 10:01:08 -0600

    ref

---
+++ src/components/mainFooter.vue
@@ -0,0 +1,6 @@
+<template lang="">
+     
+     <footer class=" h-auto bottom-0 inset-x-0 flex justify-center mt-10 bg-gray-200">
+    <img src="../imgs/ua-stackednameplate-basic.png" alt="The University of Alabama" class=" py-10">
+</footer>
+</template>
