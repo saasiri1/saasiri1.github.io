2b22a0a77e4cee99c1ef9908bd3e397ebd7fb8bc
Samb55 <samb_55@hotmail.com>
2023-12-06 20:31:22 -0600

    ref

--- src/views/resume.vue
+++ src/views/resume.vue
@@ -16,14 +16,14 @@ title{
         <ul class="list-disc">
             <li>Web developer, 2014 -2016</li>
             <li>Lecturer,
-                <ul class="list-decimal">
+                <ul class="list-disc ml-10">
                     <li>King Khalid University,</li>
                     <li>Saudi Arabia,</li>
                     <li>2015 - Present,</li>
                 </ul>
             </li>
             <li>Member of&nbsp;The High Performance Data Analytics and Computing (HiPDAC) Lab&nbsp;
-                <ul class="list-decimal">
+                <ul class="list-disc ml-10">
                 <li>The University of Alabama,&nbsp;</li>
                 <li>2019-2020,</li>
             </ul>
@@ -31,13 +31,13 @@ title{
         </ul>
         <ul class="list-disc">
             <li>Member of Geospatial data analysis&nbsp;lab&nbsp;
-                <ul  class="list-decimal">
+                <ul  class="list-disc ml-10">
                      <li>The University of Alabama,</li>
                     <li>2020-2021,</li>
                 </ul>
             </li>
     <li>Member of W4NET lab with Dr. Yang Xiao&nbsp;
-        <ul  class="list-decimal">
+        <ul  class="list-disc ml-10">
         <li>The University of Alabama,&nbsp;</li>
         <li>2021-Present&#8203;</li>
     </ul>
@@ -48,7 +48,7 @@ title{
     <h2 class="text-2xl font-bold">Education:</h2>
 
     <div class="ml-10">
-        <ul  class="list-decimal">
+        <ul  class="list-disc">
             <li>BA, Computer information Systems , King Khalid University, Saudi Arabia 2010 - 2014,</li>
             <li>MS, Computer information Analytics, Gannon University, Erie PA USA 2017 - 2019,</li>
             <li>PhD Student , Computer Science, The university of Alabama, 2019 - Present&nbsp;</li></ul>
@@ -57,7 +57,7 @@ title{
     <h2 class="text-2xl font-bold">Research Interest:</h2>
 
     <div class="ml-10">
-        <ul class="list-decimal">
+        <ul class="list-disc ">
             <li>Phishing detection,</li>
             <li>Advertisement attacks,</li>
             <li>Natural Language Processing,</li>
