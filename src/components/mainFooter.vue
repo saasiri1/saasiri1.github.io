21338708c5a2adc4ff4903cd63cbfb9132117a4d
Samb55 <samb_55@hotmail.com>
2024-02-22 00:30:15 -0600

    Remove unused files and dependencies

--- src/components/mainFooter.vue
+++
@@ -1,6 +0,0 @@
-<template lang="">
-     
-     <footer class=" h-auto bottom-0 inset-x-0 flex justify-center mt-10 bg-gray-200">
-    <img src="../imgs/ua-stackednameplate-basic.png" alt="The University of Alabama" class=" py-10">
-</footer>
-</template>
